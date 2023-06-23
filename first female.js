4793a59a55b8c7f31746adf09bd7890fa2955160
vishalkumarx <33513028+vishalkumarx@users.noreply.github.com>
2023-06-23 20:45:07 +0530

    Update first female.js

--- first female.js
+++ first female.js
@@ -106,18 +106,7 @@ const quizArray = [
   
   
 	  },
-	  {
-		"id": 8,
-		"question": "Who was the first Indian woman to win an individual Olympic gold medal?",
-		"options": [
-		  "P. V. Sindhu",
-		  "Saina Nehwal",
-		  "Mirabai Chanu",
-		  "Karnam Malleswari"
-		],
-		"description": "Karnam Malleswari won the gold medal in weightlifting at the 2000 Sydney Olympics.",
-		"correct": "Karnam Malleswari"
-	  },
+	 
 	  {
 		"id": 9,
 		"question": "Who was the first Indian woman to win the Nobel Peace Prize?",
@@ -142,18 +131,7 @@ const quizArray = [
 		"description": "Indira Gandhi, the first and only female Prime Minister of India, was posthumously awarded the Bharat Ratna in 1971.",
 		"correct": "Indira Gandhi"
 	  },
-	  {
-		"id": 11,
-		"question": "Who was the first Indian woman to win an individual Olympic bronze medal?",
-		"options": [
-		  "P. V. Sindhu",
-		  "Saina Nehwal",
-		  "Mary Kom",
-		  "Karnam Malleswari"
-		],
-		"description": "Mary Kom won the bronze medal in boxing at the 2012 London Olympics.",
-		"correct": "Mary Kom"
-	  },
+	  
 	  {
 		"id": 12,
 		"question": "Who was the first Indian woman to win the Miss Universe title?",
@@ -166,18 +144,7 @@ const quizArray = [
 		"description": "Sushmita Sen won the Miss Universe title in 1994, becoming the first Indian woman to do so.",
 		"correct": "Sushmita Sen"
 	  },
-	  {
-		"id": 13,
-		"question": "Who was the first Indian woman to win an individual Olympic gold medal?",
-		"options": [
-		  "P. V. Sindhu",
-		  "Saina Nehwal",
-		  "Mirabai Chanu",
-		  "Karnam Malleswari"
-		],
-		"description": "Karnam Malleswari won the gold medal in weightlifting at the 2000 Sydney Olympics.",
-		"correct": "Karnam Malleswari"
-	  },
+	  
 	  {
 		"id": 14,
 		"question": "Who was the first Indian woman to win the Nobel Prize in Literature?",
@@ -204,54 +171,10 @@ const quizArray = [
 		"description": "Karnam Malleswari was the first Indian woman to win the Rajiv Gandhi Khel Ratna award in 1995.",
 		"correct": "Karnam Malleswari"
 	  },
-	  {
-		"id": 16,
-		"question": "Who was the first Indian woman to win an individual Olympic silver medal?",
-		"options": [
-		  "P. V. Sindhu",
-		  "Saina Nehwal",
-		  "Mirabai Chanu",
-		  "Sakshi Malik"
-		],
-		"description": "P. V. Sindhu won the silver medal in badminton at the 2016 Rio Olympics.",
-		"correct": "P. V. Sindhu"
-	  },
-	  {
-		"id": 17,
-		"question": "Who was the first Indian woman to win the Miss World title?",
-		"options": [
-		  "Aishwarya Rai Bachchan",
-		  "Priyanka Chopra",
-		  "Sushmita Sen",
-		  "Reita Faria"
-		],
-		"description": "Reita Faria became the first Indian woman to win the Miss World title in 1966.",
-		"correct": "Reita Faria"
-	  },
-	  {
-		"id": 18,
-		"question": "Who was the first Indian woman to win an Olympic medal?",
-		"options": [
-		  "Karnam Malleswari",
-		  "P. T. Usha",
-		  "Mary Kom",
-		  "Saina Nehwal"
-		],
-		"description": "Karnam Malleswari won the bronze medal in weightlifting at the 2000 Sydney Olympics.",
-		"correct": "Karnam Malleswari"
-	  },
-	  {
-		"id": 19,
-		"question": "Who was the first Indian woman to win a Grammy Award?",
-		"options": [
-		  "Anoushka Shankar",
-		  "Lata Mangeshkar",
-		  "Shreya Ghoshal",
-		  "Tanvi Shah"
-		],
-		"description": "Tanvi Shah became the first Indian woman to win a Grammy Award in 2010 for her work on the song 'Jai Ho' from the film Slumdog Millionaire.",
-		"correct": "Tanvi Shah"
-	  },
+	  
+	  
+	  
+	  
 	  {
 		"id": 20,
 		"question": "Who was the first Indian woman to win a gold medal at the Asian Games?",
@@ -264,30 +187,8 @@ const quizArray = [
 		"description": "Kamaljit Sandhu won the gold medal in the 400m race at the 1970 Bangkok Asian Games.",
 		"correct": "Kamaljit Sandhu"
 	  },
-	  {
-		"id": 21,
-		"question": "Who was the first Indian woman to win the Booker Prize?",
-		"options": [
-		  "Arundhati Roy",
-		  "Arundhati Subramaniam",
-		  "Anita Desai",
-		  "Kiran Desai"
-		],
-		"description": "Arundhati Roy won the Booker Prize in 1997 for her novel 'The God of Small Things'.",
-		"correct": "Arundhati Roy"
-	  },
-	  {
-		"id": 22,
-		"question": "Who was the first Indian woman to win an individual Olympic gold medal?",
-		"options": [
-		  "P. V. Sindhu",
-		  "Saina Nehwal",
-		  "Mirabai Chanu",
-		  "Karnam Malleswari"
-		],
-		"description": "Karnam Malleswari won the gold medal in weightlifting at the 2000 Sydney Olympics.",
-		"correct": "Karnam Malleswari"
-	  },
+	  
+	  
 	  {
 		"id": 23,
 		"question": "Who was the first Indian woman to win the Nobel Peace Prize?",
@@ -300,18 +201,7 @@ const quizArray = [
 		"description": "Mother Teresa, an ethnic Albanian, was an Indian citizen and won the Nobel Peace Prize in 1979 for her humanitarian work.",
 		"correct": "Mother Teresa"
 	  },
-	  {
-		"id": 24,
-		"question": "Who was the first Indian woman to win the Bharat Ratna award?",
-		"options": [
-		  "Indira Gandhi",
-		  "Lata Mangeshkar",
-		  "Sarojini Naidu",
-		  "Mother Teresa"
-		],
-		"description": "Indira Gandhi, the first and only female Prime Minister of India, was posthumously awarded the Bharat Ratna in 1971.",
-		"correct": "Indira Gandhi"
-	  },
+	
 	  {
 		"id": 25,
 		"question": "Who was the first Indian woman to win an individual Olympic bronze medal?",
@@ -336,18 +226,7 @@ const quizArray = [
 		"description": "Sushmita Sen won the Miss Universe title in 1994, becoming the first Indian woman to do so.",
 		"correct": "Sushmita Sen"
 	  },
-	  {
-		"id": 27,
-		"question": "Who was the first Indian woman to win an individual Olympic gold medal?",
-		"options": [
-		  "P. V. Sindhu",
-		  "Saina Nehwal",
-		  "Mirabai Chanu",
-		  "Karnam Malleswari"
-		],
-		"description": "Karnam Malleswari won the gold medal in weightlifting at the 2000 Sydney Olympics.",
-		"correct": "Karnam Malleswari"
-	  },
+	  
 	  {
 		"id": 28,
 		"question": "Who was the first Indian woman to win the Nobel Prize in Literature?",
@@ -360,66 +239,10 @@ const quizArray = [
 		"description": "Sarojini Naidu, also known as the Nightingale of India, was the first Indian woman to win the Nobel Prize in Literature in 1913.",
 		"correct": "Sarojini Naidu"
 	  },
-	  {
-		"id": 29,
-		"question": "Who was the first Indian woman to win the Rajiv Gandhi Khel Ratna award?",
-		"options": [
-		  "Karnam Malleswari",
-		  "P. T. Usha",
-		  "Saina Nehwal",
-		  "Mary Kom"
-		],
-		"description": "Karnam Malleswari was the first Indian woman to win the Rajiv Gandhi Khel Ratna award in 1995.",
-		"correct": "Karnam Malleswari"
-	  },
-	  {
-		"id": 30,
-		"question": "Who was the first Indian woman to win an individual Olympic silver medal?",
-		"options": [
-		  "P. V. Sindhu",
-		  "Saina Nehwal",
-		  "Mirabai Chanu",
-		  "Sakshi Malik"
-		],
-		"description": "P. V. Sindhu won the silver medal in badminton at the 2016 Rio Olympics.",
-		"correct": "P. V. Sindhu"
-	  },
-	  {
-		"id": 31,
-		"question": "Who was the first Indian woman to win the Miss World title?",
-		"options": [
-		  "Aishwarya Rai Bachchan",
-		  "Priyanka Chopra",
-		  "Sushmita Sen",
-		  "Reita Faria"
-		],
-		"description": "Reita Faria became the first Indian woman to win the Miss World title in 1966.",
-		"correct": "Reita Faria"
-	  },
-	  {
-		"id": 32,
-		"question": "Who was the first Indian woman to win an Olympic medal?",
-		"options": [
-		  "Karnam Malleswari",
-		  "P. T. Usha",
-		  "Mary Kom",
-		  "Saina Nehwal"
-		],
-		"description": "Karnam Malleswari won the bronze medal in weightlifting at the 2000 Sydney Olympics.",
-		"correct": "Karnam Malleswari"
-	  },
-	  {
-		"id": 33,
-		"question": "Who was the first Indian woman to win a Grammy Award?",
-		"options": [
-		  "Anoushka Shankar",
-		  "Lata Mangeshkar",
-		  "Shreya Ghoshal",
-		  "Tanvi Shah"
-		],
-		"description": "Tanvi Shah became the first Indian woman to win a Grammy Award in 2010 for her work on the song 'Jai Ho' from the film Slumdog Millionaire.",
-		"correct": "Tanvi Shah"
-	  },
+	
+	  
+	  
+	  
 	  {
 		"id": 34,
 		"question": "Who was the first Indian woman to win a gold medal at the Asian Games?",
@@ -432,44 +255,8 @@ const quizArray = [
 		"description": "Kamaljit Sandhu won the gold medal in the 400m race at the 1970 Bangkok Asian Games.",
 		"correct": "Kamaljit Sandhu"
 	  },
-	  {
-		"id": 35,
-		"question": "Who was the first Indian woman to win the Booker Prize?",
-		"options": [
-		  "Arundhati Roy",
-		  "Arundhati Subramaniam",
-		  "Anita Desai",
-		  "Kiran Desai"
-		],
-		"description": "Arundhati Roy won the Booker Prize in 1997 for her novel 'The God of Small Things'.",
-	   
-  
-   "correct": "Arundhati Roy"
-	  },
-	  {
-		"id": 36,
-		"question": "Who was the first Indian woman to win an individual Olympic gold medal?",
-		"options": [
-		  "P. V. Sindhu",
-		  "Saina Nehwal",
-		  "Mirabai Chanu",
-		  "Karnam Malleswari"
-		],
-		"description": "Karnam Malleswari won the gold medal in weightlifting at the 2000 Sydney Olympics.",
-		"correct": "Karnam Malleswari"
-	  },
-	  {
-		"id": 37,
-		"question": "Who was the first Indian woman to win the Nobel Peace Prize?",
-		"options": [
-		  "Indira Gandhi",
-		  "Mother Teresa",
-		  "Aung San Suu Kyi",
-		  "Malala Yousafzai"
-		],
-		"description": "Mother Teresa, an ethnic Albanian, was an Indian citizen and won the Nobel Peace Prize in 1979 for her humanitarian work.",
-		"correct": "Mother Teresa"
-	  },
+	 
+	
 	  {
 		"id": 38,
 		"question": "Who was the first Indian woman to win the Bharat Ratna award?",
@@ -506,18 +293,7 @@ const quizArray = [
 		"description": "Sushmita Sen won the Miss Universe title in 1994, becoming the first Indian woman to do so.",
 		"correct": "Sushmita Sen"
 	  },
-	  {
-		"id": 41,
-		"question": "Who was the first Indian woman to win an individual Olympic gold medal?",
-		"options": [
-		  "P. V. Sindhu",
-		  "Saina Nehwal",
-		  "Mirabai Chanu",
-		  "Karnam Malleswari"
-		],
-		"description": "Karnam Malleswari won the gold medal in weightlifting at the 2000 Sydney Olympics.",
-		"correct": "Karnam Malleswari"
-	  },
+	 
 	  {
 		"id": 42,
 		"question": "Who was the first Indian woman to win the Nobel Prize in Literature?",
@@ -530,66 +306,10 @@ const quizArray = [
 		"description": "Sarojini Naidu, also known as the Nightingale of India, was the first Indian woman to win the Nobel Prize in Literature in 1913.",
 		"correct": "Sarojini Naidu"
 	  },
-	  {
-		"id": 43,
-		"question": "Who was the first Indian woman to win the Rajiv Gandhi Khel Ratna award?",
-		"options": [
-		  "Karnam Malleswari",
-		  "P. T. Usha",
-		  "Saina Nehwal",
-		  "Mary Kom"
-		],
-		"description": "Karnam Malleswari was the first Indian woman to win the Rajiv Gandhi Khel Ratna award in 1995.",
-		"correct": "Karnam Malleswari"
-	  },
-	  {
-		"id": 44,
-		"question": "Who was the first Indian woman to win an individual Olympic silver medal?",
-		"options": [
-		  "P. V. Sindhu",
-		  "Saina Nehwal",
-		  "Mirabai Chanu",
-		  "Sakshi Malik"
-		],
-		"description": "P. V. Sindhu won the silver medal in badminton at the 2016 Rio Olympics.",
-		"correct": "P. V. Sindhu"
-	  },
-	  {
-		"id": 45,
-		"question": "Who was the first Indian woman to win the Miss World title?",
-		"options": [
-		  "Aishwarya Rai Bachchan",
-		  "Priyanka Chopra",
-		  "Sushmita Sen",
-		  "Reita Faria"
-		],
-		"description": "Reita Faria became the first Indian woman to win the Miss World title in 1966.",
-		"correct": "Reita Faria"
-	  },
-	  {
-		"id": 46,
-		"question": "Who was the first Indian woman to win an Olympic medal?",
-		"options": [
-		  "Karnam Malleswari",
-		  "P. T. Usha",
-		  "Mary Kom",
-		  "Saina Nehwal"
-		],
-		"description": "Karnam Malleswari won the bronze medal in weightlifting at the 2000 Sydney Olympics.",
-		"correct": "Karnam Malleswari"
-	  },
-	  {
-		"id": 47,
-		"question": "Who was the first Indian woman to win a Grammy Award?",
-		"options": [
-		  "Anoushka Shankar",
-		  "Lata Mangeshkar",
-		  "Shreya Ghoshal",
-		  "Tanvi Shah"
-		],
-		"description": "Tanvi Shah became the first Indian woman to win a Grammy Award in 2010 for her work on the song 'Jai Ho' from the film Slumdog Millionaire.",
-		"correct": "Tanvi Shah"
-	  },
+	  
+	 
+	 
+	
 	  {
 		"id": 48,
 		"question": "Who was the first Indian woman to win a gold medal at the Asian Games?",
@@ -602,30 +322,8 @@ const quizArray = [
 		"description": "Kamaljit Sandhu won the gold medal in the 400m race at the 1970 Bangkok Asian Games.",
 		"correct": "Kamaljit Sandhu"
 	  },
-	  {
-		"id": 49,
-		"question": "Who was the first Indian woman to win the Booker Prize?",
-		"options": [
-		  "Arundhati Roy",
-		  "Arundhati Subramaniam",
-		  "Anita Desai",
-		  "Kiran Desai"
-		],
-		"description": "Arundhati Roy won the Booker Prize in 1997 for her novel 'The God of Small Things'.",
-		"correct": "Arundhati Roy"
-	  },
-	  {
-		"id": 50,
-		"question": "Who was the first Indian woman to win an individual Olympic gold medal?",
-		"options": [
-		  "P. V. Sindhu",
-		  "Saina Nehwal",
-		  "Mirabai Chanu",
-		  "Karnam Malleswari"
-		],
-		"description": "Karnam Malleswari won the gold medal in weightlifting at the 2000 Sydney Olympics.",
-		"correct": "Karnam Malleswari"
-	  }
+	 
+	 
 ]
    ;
 
